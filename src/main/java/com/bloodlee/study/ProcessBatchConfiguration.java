e34975684b3d15e4a5df285a3a70a38eeb1a792e
bloodavenger <l.seungsu@gmail.com>
2018-05-10 10:54:29 +0900

    step test

--- src/main/java/com/bloodlee/study/ProcessBatchConfiguration.java
+++ src/main/java/com/bloodlee/study/ProcessBatchConfiguration.java
@@ -4,16 +4,20 @@ import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,6 +35,45 @@ public class ProcessBatchConfiguration {
 
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1, Step step2) {
+    	log.info("importUserJob");
+        return jobBuilderFactory.get("importUserJob")
+            .incrementer(new RunIdIncrementer())
+            .listener(listener)
+            .flow(step1)
+            .next(step2)
+            .end()
+            .build();
+    }
+
+    @Bean
+    public Step step1(JdbcBatchItemWriter<Person> writer) {
+    	log.info("step1");
+        return stepBuilderFactory.get("step1")
+            .<Person, Person> chunk(10)
+            .reader(reader())
+            .processor(processor())
+            .writer(writer)
+            .build();
+    }
+
+    @Bean
+    public Step step2() {
+    	log.info("step2");
+    	Tasklet tasklet = new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				// TODO Auto-generated method stub
+				log.info("step2 tasklet");
+				return RepeatStatus.FINISHED;
+			}
+		};
+        return stepBuilderFactory.get("step2")
+        		.tasklet(tasklet)
+        		.build();
+    }
 
     @Bean
     public FlatFileItemReader<Person> reader() {
@@ -62,23 +105,4 @@ public class ProcessBatchConfiguration {
             .build();
     }
 
-    @Bean
-    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
-        return jobBuilderFactory.get("importUserJob")
-            .incrementer(new RunIdIncrementer())
-            .listener(listener)
-            .flow(step1)
-            .end()
-            .build();
-    }
-
-    @Bean
-    public Step step1(JdbcBatchItemWriter<Person> writer) {
-        return stepBuilderFactory.get("step1")
-            .<Person, Person> chunk(10)
-            .reader(reader())
-            .processor(processor())
-            .writer(writer)
-            .build();
-    }
 }
